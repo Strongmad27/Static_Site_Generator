80f58adfc51062442d120d3da06d2d4d53b15ded
Strongmad27 <keller.bpk@gmail.com>
2025-06-04 10:32:15 -0500

    finished block to type

--- src/test_block_to_type.py
+++ src/test_block_to_type.py
@@ -4,7 +4,43 @@ from md_to_block import markdown_to_blocks
 
 class TestTextTo(unittest.TestCase):
     def test_text_to_1(self):
-        text = "```\nsome code\n\nmore code\n```"
+        text = "```\nprint('hello')\n```"
         markdown = markdown_to_blocks(text)
         test_bt = block_to_block_type(markdown[0])
         self.assertIs(BlockType.CODE, test_bt)
+
+    def test_text_to_2(self):
+        text = "# ```\nprint('hello')\n```"
+        markdown = markdown_to_blocks(text)
+        test_bt = block_to_block_type(markdown[0])
+        self.assertIs(BlockType.HEADING, test_bt)
+
+    def test_text_to_3(self):
+        text = "###### ```\nprint('hello')\n```"
+        markdown = markdown_to_blocks(text)
+        test_bt = block_to_block_type(markdown[0])
+        self.assertIs(BlockType.HEADING, test_bt)
+
+    def test_text_to_4(self):
+        text = "> \n> ```\n>print('hello')\n>```"
+        markdown = markdown_to_blocks(text)
+        test_bt = block_to_block_type(markdown[0])
+        self.assertIs(BlockType.QUOTE, test_bt)
+
+    def test_text_to_5(self):
+        text = "- \n- ```\n- print('hello')\n- ```"
+        markdown = markdown_to_blocks(text)
+        test_bt = block_to_block_type(markdown[0])
+        self.assertIs(BlockType.UNORDERED_LIST, test_bt)
+
+    def test_text_to_6(self):
+        text = "1. \n2. ```\n3. print('hello')\n4. ```"
+        markdown = markdown_to_blocks(text)
+        test_bt = block_to_block_type(markdown[0])
+        self.assertIs(BlockType.ORDERED_LIST, test_bt)
+
+    def test_text_to_7(self):
+        text = "1. \n2. ```\n3. print('hello')\n5. ```"
+        markdown = markdown_to_blocks(text)
+        test_bt = block_to_block_type(markdown[0])
+        self.assertIsNot(BlockType.QUOTE, test_bt)

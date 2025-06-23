e9e70621a78d1825b408d11a0f3847c2dd8630da
Strongmad27 <keller.bpk@gmail.com>
2025-06-23 08:43:30 -0500

    to push

--- src/generate_web.py
+++ src/generate_web.py
@@ -29,5 +29,5 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, 'w') as file:
         file.write(new_html)
             
-
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
                 
